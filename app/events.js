25cec441edbe5b35fbf291009c9e6edd405cd5e3
Jose <jmartinez@openfinance.es>
2013-03-25 17:10:05 +0100

    events js

--- app/events.js
+++ app/events.js
@@ -1,13 +1,15 @@
-Lungo.Events.init({
-		'load section#carousel': function(event) {
-				var el = $$('[data-control=carousel]')[0];
-        App.carousel = Lungo.Element.Carousel(el, function(index, element) {
-            Lungo.dom("section#carousel .title span").html(index + 1);
-        });
-    },    
-    'tap section#carousel > header#left':  
-    	App.carousel.prev,
+var el = $$('[data-control=carousel]')[0];
+carousel = Lungo.Element.Carousel(el, function(index, element) {
+    Lungo.dom("section#carousel .title span").html(index + 1);
+});
 
-    'tap section#carousel > header#right': 
-    	App.carousel.next
+Lungo.Events.init({
+    'tap section#carousel > header a[data-direction="left"]': function(){
+        //carousel.prev;
+        alert("1");
+    },
+    'tap section#carousel > header a[data-direction="right"]': function(){
+        //carousel.next;
+        alert("2");
+    }
 });
