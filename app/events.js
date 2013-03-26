e02838eb46f708da501dfa8c10142671e503686f
Jose <jmartinez@openfinance.es>
2013-03-26 10:51:40 +0100

    carousl events

--- app/events.js
+++ app/events.js
@@ -1,15 +1,15 @@
 var el = $$('[data-control=carousel]')[0];
-carousel = Lungo.Element.Carousel(el, function(index, element) {
-    Lungo.dom("section#carousel .title span").html(index + 1);
+carousel = Lungo.Element.Carousel(el, function(index, element) {    
+    //alert("Position: " + index);
 });
 
 Lungo.Events.init({
-    'tap section#carousel > header a[data-direction="left"]': function(){
-        //carousel.prev;
-        alert("1");
+    'tap section#carousel > article a[data-direction="left"]': function(){
+        carousel.prev();
+
     },
-    'tap section#carousel > header a[data-direction="right"]': function(){
-        //carousel.next;
-        alert("2");
+    'tap section#carousel > article a[data-direction="right"]': function(){
+        carousel.next();
     }
 });
+
