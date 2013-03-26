9625cb020a5c9a64ee28860127353b09663f9c55
Jose <jmartinez@openfinance.es>
2013-03-26 11:56:24 +0100

    bullet position

--- app/events.js
+++ app/events.js
@@ -1,6 +1,9 @@
 var el = $$('[data-control=carousel]')[0];
 carousel = Lungo.Element.Carousel(el, function(index, element) {    
     //alert("Position: " + index);
+    Lungo.dom("[data-control='carousel'] > ul li.selected").removeClass();
+    var selector = "[data-control='carousel'] > ul li:nth-child("+(index+2)+")";
+    Lungo.dom(selector).addClass("selected");
 });
 
 Lungo.Events.init({
